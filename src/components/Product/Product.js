16e5fd033ac9365c101c570bd607a85ea55a8de5
Jan Kędzierski <jkedzierski087@gmail.com>
2023-03-02 16:23:34 +0100

    add prepareColorClassName function and map colors

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,22 +5,20 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 const Product = (props) => {
-
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
 
   console.log('currentColor: ', currentColor);
   console.log('currentSize: ', currentSize);
 
- 
+  const prepareColorClassName = (color) => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  };
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
-        <img 
-          className={styles.image}
-          alt={`${props.title} - ${currentColor} - ${currentSize}`}
-          size={currentSize}
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg` } />
+        <img className={styles.image} alt={`${props.title} - ${currentColor} - ${currentSize}`} size={currentSize} src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
@@ -31,18 +29,30 @@ const Product = (props) => {
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              <li>
+                <button type="button" className={styles.active}>
+                  S
+                </button>
+              </li>
+              <li>
+                <button type="button">M</button>
+              </li>
+              <li>
+                <button type="button">L</button>
+              </li>
+              <li>
+                <button type="button">XL</button>
+              </li>
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {props.colors.map((item) => (
+                <li key={item}>
+                  <button type="button" className={clsx(prepareColorClassName(item), item === item && styles.active)} />
+                </li>
+              ))}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -51,16 +61,20 @@ const Product = (props) => {
         </form>
       </div>
     </article>
-  )
+  );
 };
 
-Product.propTypes = {
-  id : PropTypes.number.isRequired,
-  name : PropTypes.string.isRequired,
-  title : PropTypes.string.isRequired,
-  sizes : PropTypes.array.isRequired,
-  colors : PropTypes.array.isRequired,
-  basePrice : PropTypes.number.isRequired,
+{
+  /* <li><button type="button" className={clsx(styles.colorBlack,styles.active )} /></li> */
 }
+
+Product.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  sizes: PropTypes.array.isRequired,
+  colors: PropTypes.array.isRequired,
+  basePrice: PropTypes.number.isRequired,
+};
 
 export default Product;
